4d49f757d9ab1f24bb65abb40ccc7ccf2861260d
aetherdrive <verdensbestemarcus@gmail.com>
2026-01-28 12:04:09 +0100

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 10000;
 
 app.disable("x-powered-by");
 app.use(express.json({ limit: "100kb" }));
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  return next();
+});
 
 let cachedMetrics = null;
 let cachedAt = 0;
